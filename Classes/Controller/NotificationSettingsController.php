179c4fdd7f0df845028367ca9ffc6dec25590f84
Sebastian Michaelsen <sebastian@michaelsen.io>
2022-06-02 14:51:02 +0200

    list available events

--- Classes/Controller/NotificationSettingsController.php
+++ Classes/Controller/NotificationSettingsController.php
@@ -9,23 +9,35 @@ use Smichaelsen\Noti\Event\EventRegistry;
 use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Core\Http\HtmlResponse;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3Fluid\Fluid\View\ViewInterface;
 
 class NotificationSettingsController
 {
     private EventRegistry $eventRegistry;
     private ModuleTemplate $moduleTemplate;
 
-    public function __construct()
+    public function __construct(EventRegistry $eventRegistry)
     {
-        $this->eventRegistry = GeneralUtility::makeInstance(EventRegistry::class);
+        $this->eventRegistry = $eventRegistry;
         $this->moduleTemplate = GeneralUtility::makeInstance(ModuleTemplate::class);
     }
 
     public function subscriptionsAction(): ResponseInterface
     {
-        $events = $this->eventRegistry->getEvents();
-        $content = '<h1>Manage your subscriptions</h1>';
-        $this->moduleTemplate->setContent($content);
+        $view = $this->createView();
+        $view->assign('events', $this->eventRegistry->getEvents());
+        $this->moduleTemplate->setContent($view->render('Subscriptions'));
         return new HtmlResponse($this->moduleTemplate->renderContent());
+    }
+
+    protected function createView(): ViewInterface
+    {
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplateRootPaths(['EXT:noti/Resources/Private/Templates']);
+        $view->setPartialRootPaths(['EXT:noti/Resources/Private/Partials']);
+        $view->setLayoutRootPaths(['EXT:noti/Resources/Private/Layouts']);
+        $view->getRequest()->setControllerExtensionName('Noti');
+        return $view;
     }
 }
--- Classes/Service/NotificationService.php
+++ Classes/Service/NotificationService.php
@@ -1,75 +1,13 @@
 <?php
 namespace Smichaelsen\Noti\Service;
 
-use Smichaelsen\Noti\Domain\Model\Event;
 use Smichaelsen\Noti\Event\EventInterface;
-use Smichaelsen\Noti\Notifier\NotifierInterface;
-use TYPO3\CMS\Core\Database\Connection;
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\SingletonInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class NotificationService implements SingletonInterface
 {
-    private Connection $connection;
-    private LanguageService $languageService;
-
-    public function __construct(ConnectionPool $connectionPool, LanguageService $languageService)
-    {
-        $this->connection = $connectionPool->getConnectionForTable('tx_noti_subscription');
-        $this->languageService = $languageService;
-    }
-
     public function __invoke(EventInterface $event): EventInterface
     {
-        $subscriptions = $this->loadSubscriptions();
-        foreach ($subscriptions as $subscription) {
-            $notifier = $this->getNotifierForSubscription($subscription);
-            $variables['eventTitle'] = $this->languageService->sL($notification->getTitle());
-            $variables['notifier'] = $notifier;
-            $variables['notifierClassName'] = get_class($notifier);
-            $notifier->notify($event, $subscription, $variables);
-        }
-        return $notification;
-    }
-
-    public function triggerEvent(string $identifier, array $variables = []): void
-    {
-        $this->notify($this->eventRegistry->getEvent($identifier), $variables);
-    }
-
-    /**
-     * @param Event $event
-     * @param array $variables
-     */
-    public function notify(Event $event, array $variables): void
-    {
-        $subscriptions = $this->loadSubscriptions($event);
-        foreach ($subscriptions as $subscription) {
-            $notifier = $this->getNotifierForSubscription($subscription);
-            $variables['eventTitle'] = $this->languageService->sL($event->getTitle());
-            $variables['notifier'] = $notifier;
-            $variables['notifierClassName'] = get_class($notifier);
-            $notifier->notify($event, $subscription, $variables);
-        }
-    }
-
-    protected function loadSubscriptions(Event $event): array
-    {
-        return $this->connection->select(
-            ['*'],
-            'tx_noti_subscription',
-            ['event', $event->getIdentifier()]
-        )->fetchAllAssociative();
-    }
-
-    protected function getNotifierForSubscription(array $subscription): NotifierInterface
-    {
-        $notifier = GeneralUtility::makeInstance($subscription['type']);
-        if (!$notifier instanceof NotifierInterface) {
-            throw new \Exception($subscription['type'] . ' doesn\'t implement the NotifierInterface', 1475737136);
-        }
-        return $notifier;
+        return $event;
     }
 }
