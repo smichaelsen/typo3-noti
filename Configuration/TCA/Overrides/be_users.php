3254bb25853be7f1b60b130b1bd4d6397d928125
Sebastian Michaelsen <sebastian@michaelsen.io>
2023-12-18 09:42:00 +0100

    defined('TYPO3') or die();

--- Configuration/TCA/Overrides/be_users.php
+++ Configuration/TCA/Overrides/be_users.php
@@ -1,8 +1,6 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
 $GLOBALS['TCA']['be_users']['columns']['userMods']['config']['items'] = [
     ['Notification Settings', 'user_notification_settings', 'actions-cog-alt', null, ['title' => 'Notification Settings', 'description' => '']],
