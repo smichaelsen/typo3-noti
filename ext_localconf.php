32d1cb2623f1d53f879b34c1d067cff44f7d623c
Amir Arends <amir.arends@gmail.com>
2024-01-18 06:42:06 +0100

    [BUGFIX] Move icons registration to Icons.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,34 @@
 <?php
 
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][\Psr\Log\LogLevel::EMERGENCY][\Smichaelsen\Noti\Log\DispatchLogNotification::class] = [];
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][\Psr\Log\LogLevel::ALERT][\Smichaelsen\Noti\Log\DispatchLogNotification::class] = [];
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][\Psr\Log\LogLevel::CRITICAL][\Smichaelsen\Noti\Log\DispatchLogNotification::class] = [];
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][\Psr\Log\LogLevel::ERROR][\Smichaelsen\Noti\Log\DispatchLogNotification::class] = [];
+use Psr\Log\LogLevel;
+use Smichaelsen\Noti\Backend\Toolbar\NotificationCenterToolbarItem;
+use Smichaelsen\Noti\Log\DispatchLogNotification;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask;
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1654079048] = \Smichaelsen\Noti\Backend\Toolbar\NotificationCenterToolbarItem::class;
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][LogLevel::EMERGENCY][DispatchLogNotification::class] = [];
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][LogLevel::ALERT][DispatchLogNotification::class] = [];
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][LogLevel::CRITICAL][DispatchLogNotification::class] = [];
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['processorConfiguration'][LogLevel::ERROR][DispatchLogNotification::class] = [];
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables']['tx_noti_notification'] = [
+$GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1654079048] = NotificationCenterToolbarItem::class;
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][TableGarbageCollectionTask::class]['options']['tables']['tx_noti_notification'] = [
     'dateField' => 'tstamp',
     'expirePeriod' => 30,
 ];
 
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'ext-noti-bell',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:noti/Resources/Public/Icons/bell.svg')]
-);
+$currentTypo3Version = VersionNumberUtility::getCurrentTypo3Version();
+if (version_compare($currentTypo3Version, '11.5.0', '<')) {
+    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'ext-noti-bell',
+        SvgIconProvider::class,
+        ['source' => GeneralUtility::getFileAbsFileName('EXT:noti/Resources/Public/Icons/bell.svg')]
+    );
+}
