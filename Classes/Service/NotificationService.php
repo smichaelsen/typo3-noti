e88881822b00423414c491fa28caf774aaf2517c
Amir Arends <amir.arends@gmail.com>
2024-02-23 12:28:23 +0100

    [FEATURE] Integrate extension setting to disable email notifications

--- Classes/Service/NotificationService.php
+++ Classes/Service/NotificationService.php
@@ -3,7 +3,9 @@
 namespace Smichaelsen\Noti\Service;
 
 use Smichaelsen\Noti\Event\EventInterface;
+use Smichaelsen\Noti\Notifier\EmailNotifier;
 use Smichaelsen\Noti\Notifier\NotifierInterface;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Database\Connection;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\SingletonInterface;
@@ -12,11 +14,17 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class NotificationService implements SingletonInterface
 {
     private Connection $connection;
+    private ExtensionConfiguration $extensionConfiguration;
     private NotifierRegistry $notifierRegistry;
 
-    public function __construct(ConnectionPool $connectionPool, NotifierRegistry $notifierRegistry)
+    public function __construct(
+        ConnectionPool $connectionPool,
+        ExtensionConfiguration $extensionConfiguration,
+        NotifierRegistry $notifierRegistry
+    )
     {
         $this->connection = $connectionPool->getConnectionForTable('tx_noti_subscription');
+        $this->extensionConfiguration = $extensionConfiguration;
         $this->notifierRegistry = $notifierRegistry;
     }
 
@@ -25,6 +33,12 @@ class NotificationService implements SingletonInterface
         $eventKey = get_class($event) . '\\' . ($event->getVariant() ?? '_default');
         $subscriptions = $this->loadSubscriptionsForEventKey($eventKey);
         foreach ($subscriptions as $subscription) {
+            if (
+                $subscription['notifier_key'] === get_class(new EmailNotifier()) &&
+                $this->extensionConfiguration->get('noti', 'configuration/disableEmailNotifications')
+            ) {
+                continue;
+            }
             if (!in_array($subscription['notifier_key'], $this->notifierRegistry->getAvailableNotifierKeys())) {
                 continue;
             }
